b9ae954f2ea2b52ccd4f262b34746eafd1a12bee
Kasheftin <Kasheftin@gmail.com>
2021-11-07 15:12:19 +0200

    Initial Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as example2 from './example2'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    example2: {
+      ...example2,
+      namespaced: true
+    }
   }
 })
